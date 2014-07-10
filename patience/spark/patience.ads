71675b70907ba8167feceb596fc2718fe743d695
Claude Marché <Claude.Marche@inria.fr>
2014-07-10 16:21:32 +0200

    preliminary attempt to code patience game in Spark

---
+++ patience/spark/patience.ads
@@ -0,0 +1,9 @@
+
+package Patience with
+  SPARK_Mode
+is
+
+   type Card is range 1..52;
+   type Stack is array (Positive range <>) of Card;
+
+end Patience;
---
+++ patience/spark/test_patience.adb
@@ -0,0 +1,11 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Patience; use Patience;
+
+procedure Test_Patience is
+   
+   Input : Stack := (9, 7, 10, 9, 5, 4, 10);
+     
+begin
+   Put_Line ("Test of a patience game");
+   
+end Test_Patience;
